22d099cbddbd181e68c74144e307baea578f76f8
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2024-02-27 20:11:58 +0100

    small refactor

--- pygithooks.py
+++ pygithooks.py
@@ -13,6 +13,31 @@ import rich
 import rich.console
 import rich.theme
 
+FILE = Path(__file__).absolute()
+
+HOOK_TEMPLATE = R"""#!/bin/sh
+set -eu
+
+sys_exe={sys_exe}
+if command -v "$sys_exe" 1>/dev/null 2>&1 ; then
+    exec "$sys_exe" {pygithooks} run {hook} -- "$@"
+fi
+
+echo "pygithooks: python ($sys_exe) not found, no hooks running" 1>&2
+exit 0
+"""
+
+_THEME = rich.theme.Theme(
+    {
+        "info": "blue",
+        "success": "green",
+        "skip": "yellow",
+        "fail": "red",
+    }
+)
+
+_PGH = "[dim bold]pygithooks[/dim bold]:"
+
 
 def split_args(*arg_groups: Union[str, List[Any]]) -> List[str]:
     return [
@@ -22,27 +47,11 @@ def split_args(*arg_groups: Union[str, List[Any]]) -> List[str]:
     ]
 
 
-_THEME = rich.theme.Theme(
-    {
-        "pgh": "dim bold",
-        "info": "blue",
-        "succ": "green",
-        "skip": "yellow",
-        "fail": "red",
-        "bold_succ": "bold green",
-        "bold_skip": "bold yellow",
-        "bold_fail": "bold red",
-    }
-)
-
-_PGH = "[pgh]pygithooks[/pgh]:"
-
-
 @dataclass
 class Ctx:
     argv: List[str] = field(default_factory=lambda: sys.argv)
     cwd: Path = field(default_factory=Path.cwd)
-    env: Dict[str, str] = field(default_factory=lambda: os.environ)
+    env: Dict[str, str] = field(default_factory=lambda: dict(os.environ))
     stdin: TextIO = field(default_factory=lambda: sys.stdin)
     stdout: TextIO = field(default_factory=lambda: sys.stdout)
     stderr: TextIO = field(default_factory=lambda: sys.stderr)
@@ -51,7 +60,7 @@ class Ctx:
     )
 
     def msg(self, *args, **kwargs):
-        self.console.print(*args, **kwargs)
+        self.console.print(_PGH, *args, **kwargs)
 
     def out(self, *args, **kwargs):
         kwargs.setdefault("file", self.stderr)
@@ -66,23 +75,8 @@ class Ctx:
             kwargs.setdefault("stdin", self.stdin)
             kwargs.setdefault("stdout", self.stdout)
             kwargs.setdefault("stderr", self.stderr)
-        args = split_args(*args)
-        return subprocess.run(args, **kwargs)
-
-
-FILE = Path(__file__).absolute()
-
-HOOK_TEMPLATE = R"""#!/bin/sh
-set -eu
-
-sys_exe={sys_exe}
-if command -v "$sys_exe" 1>/dev/null 2>&1 ; then
-    exec "$sys_exe" {pygithooks} run {hook} -- "$@"
-fi
-
-echo "python ($sys_exe) not found, pygithooks not running" 1>&2
-exit 0
-"""
+        args_list = split_args(*args)
+        return subprocess.run(args_list, **kwargs)
 
 
 @dataclass
@@ -94,24 +88,22 @@ class GitHook:
 @dataclass
 class GitHookScript:
     git_hook: GitHook
-    path: Path
+    name: str
     path_full: Path
 
 
 @dataclass
 class CompletedGitHookScript:
     git_hook_script: GitHookScript
-    completed_process: subprocess.CompletedProcess
+    completed_process: Optional[subprocess.CompletedProcess]
 
-    @staticmethod
-    def run(ctx: Ctx, git_hook_script: GitHookScript) -> "CompletedGitHookScript":
-        return CompletedGitHookScript(
-            git_hook_script, ctx.run([git_hook_script.path_full], check=False, capture_output=True)
-        )
+    @property
+    def skipped(self) -> bool:
+        return self.completed_process is None
 
     @property
     def succeeded(self) -> bool:
-        return self.completed_process.returncode == 0
+        return self.completed_process is not None and self.completed_process.returncode == 0
 
 
 GIT_HOOKS: Dict[str, GitHook] = {
@@ -124,49 +116,66 @@ class PyGitHooks:
     ctx: Ctx
     parser: argparse.ArgumentParser
 
-    def main(self):
+    def main(self) -> None:
         args = vars(self.parser.parse_args(self.ctx.argv[1:]))
+        chdir: Path
+        if chdir := args.pop("chdir", None):
+            os.chdir(chdir)
+
         action = args.pop("action")
         action(self, **args)
 
     def help(self):
         self.parser.print_help(self.ctx.stderr)
 
-    def run(self, *, hook: str):
-        self.ctx.msg(_PGH, f"[bold]{hook}[/bold] hooks running...", style="info")
-        self.env_path = self.env_path_with_sys_exe_prefix
-        results = [
-            CompletedGitHookScript.run(self.ctx, script)
-            for script in self.git_hook_scripts(GIT_HOOKS[hook])
-        ]
+    def _run_git_hook_script(self, git_hook_script: GitHookScript) -> CompletedGitHookScript:
+        completed_process: Optional[subprocess.CompletedProcess] = None
+        try:
+            completed_process = self.ctx.run(
+                [git_hook_script.path_full], check=False, capture_output=True
+            )
+        except OSError:
+            pass
 
-        for result in results:
+        return CompletedGitHookScript(git_hook_script, completed_process)
+
+    def run(self, *, hook: str):
+        self.ctx.msg(f"[bold]{hook}[/bold] hooks running...", style="info")
+        self.env_path = self.env_path_with_sys_exe_prefix
+
+        results: List[CompletedGitHookScript] = []
+        for git_hook_script in self.git_hook_scripts(GIT_HOOKS[hook]):
+            result = self._run_git_hook_script(git_hook_script)
+            results.append(result)
+
             if result.succeeded:
                 self.ctx.msg(
-                    _PGH,
-                    f"[bold]{result.git_hook_script.path}[/bold]: [bold]OK[/bold]",
-                    style="succ",
+                    f"[bold]{result.git_hook_script.name}[/bold]: [bold]OK[/bold]", style="success"
+                )
+            elif result.skipped:
+                self.ctx.msg(
+                    f"[bold]{result.git_hook_script.name}[/bold]: [bold]SKIPPED[/bold]",
+                    style="skip",
                 )
             else:
                 self.ctx.msg(
-                    _PGH,
-                    f"[bold]{result.git_hook_script.path}[/bold]: [bold]FAILED[/bold]",
-                    style="fail",
+                    f"[bold]{result.git_hook_script.name}[/bold]: [bold]FAILED[/bold]", style="fail"
                 )
 
-            self.ctx.stderr.write(result.completed_process.stderr)
-            self.ctx.stdout.write(result.completed_process.stdout)
+            if result.completed_process:
+                self.ctx.stderr.write(result.completed_process.stderr)
+                self.ctx.stdout.write(result.completed_process.stdout)
 
-        all_succeeded = all(result.succeeded for result in results)
+        all_succeeded = all(result.succeeded or result.skipped for result in results)
         any_succeeded = any(result.succeeded for result in results)
         if any_succeeded and all_succeeded:
-            self.ctx.msg(_PGH, f"{hook} hooks SUCCEEDED", style="bold_succ")
+            self.ctx.msg(f"{hook} hooks SUCCEEDED", style="bold green")
             sys.exit(0)
         elif all_succeeded:
-            self.ctx.msg(_PGH, f"{hook} hooks SKIPPED", style="bold_skip")
+            self.ctx.msg(f"{hook} hooks SKIPPED", style="bold yellow")
             sys.exit(0)
         else:
-            self.ctx.msg(_PGH, f"{hook} hooks FAILED", style="bold_fail")
+            self.ctx.msg(f"{hook} hooks FAILED", style="bold red")
             sys.exit(1)
 
     def install(self):
@@ -186,11 +195,12 @@ class PyGitHooks:
         top_level = Path(self.pygithooks_path / git_hook.name)
         if top_level.is_dir():
             yield from [
-                GitHookScript(git_hook, path.relative_to(self.pygithooks_path), path)
+                GitHookScript(git_hook, path.relative_to(self.pygithooks_path).as_posix(), path)
                 for path in sorted(top_level.iterdir())
+                if path.stat().st_mode & stat.S_IEXEC == stat.S_IEXEC
             ]
 
-    def git(self, *args, **kwargs) -> subprocess.CompletedProcess:
+    def run_git(self, *args, **kwargs) -> subprocess.CompletedProcess:
         return self.ctx.run("git", *args, **kwargs)
 
     @property
@@ -207,12 +217,12 @@ class PyGitHooks:
 
     @cached_property
     def git_dir(self) -> Path:
-        return Path(self.git("rev-parse --git-dir", capture_output=True).stdout.strip())
+        return Path(self.run_git("rev-parse --git-dir", capture_output=True).stdout.strip())
 
     @cached_property
     def git_hooks_path(self) -> Path:
         return Path(
-            self.git(
+            self.run_git(
                 "config --get --default",
                 [self.git_dir / "hooks"],
                 "core.hooksPath",
@@ -223,7 +233,7 @@ class PyGitHooks:
     @cached_property
     def git_top_level(self) -> Path:
         return Path(
-            self.git("rev-parse --show-toplevel", capture_output=True).stdout.strip()
+            self.run_git("rev-parse --show-toplevel", capture_output=True).stdout.strip()
         ).absolute()
 
     @cached_property
@@ -239,6 +249,7 @@ def main(ctx: Optional[Ctx] = None):
         allow_abbrev=False,
     )
     parser.set_defaults(action=PyGitHooks.help)
+    parser.add_argument("--chdir", "-C", metavar="DIR", type=Path, help="chdir to this directory")
     subparsers = parser.add_subparsers(title="Commands")
 
     parser_run = subparsers.add_parser(
